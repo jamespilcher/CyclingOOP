b0b5b8e91e0d7fe9e8a5af0fa87de556809e64e2
jamespilcher <90052874+jamespilcher@users.noreply.github.com>
2022-03-03 18:47:08 +0000

    current project

---
+++ cycling/Race.java
@@ -0,0 +1,24 @@
+package cycling;
+import java.util.ArrayList;
+class Race {
+    private int raceID;
+    private String name;
+    private String description;
+
+    ArrayList<Stage> stages = new ArrayList<Stage>();
+    private static int numberOfRaces = 0;
+
+
+
+    public Race(String name, String description){
+        this.name = name;
+        this.description = description;
+        this.raceID = ++numberOfRaces;
+    }
+
+    public ArrayList<Stage> getSegments() { return stages; }
+    public int getRaceID() { return raceID; }
+    public String getName() { return name; }
+    public String getDescription() { return description; }
+    
+}
