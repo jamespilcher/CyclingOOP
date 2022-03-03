ab71137e1130070de50c1e0cada6236eeef3f646
jamespilcher <90052874+jamespilcher@users.noreply.github.com>
2022-03-03 18:48:46 +0000

    Current source code files

---
+++ cycling/ClimbSegment.java
@@ -0,0 +1,15 @@
+package cycling;
+public class ClimbSegment extends Segment {
+    private double averageGradient;
+    private double length;
+
+    public ClimbSegment(int stageID, SegmentType type, double location, double averageGradient, double length){
+        super(stageID, type, location);
+        this.averageGradient = averageGradient;
+        this.length = length;
+    }
+
+    public double getAverageGradient() {return averageGradient;}
+    public double getLength() {return length;}
+    
+}
