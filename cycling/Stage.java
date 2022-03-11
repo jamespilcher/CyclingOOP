bdf3f6c8a13bd3ca8619861eba59e08f91c2dfec
Bahkbar <73535967+Bahkbar@users.noreply.github.com>
2022-03-11 11:36:02 +0000

    Committed removeStage

--- cycling/Stage.java
+++ cycling/Stage.java
@@ -30,6 +30,10 @@ class Stage {
     public void addSegment(Segment segment){
         segments.add(segment);
     }
+    
+    public void removeStage(Stage stage){
+        stages.remove(stage);
+    }
 
     public ArrayList<Segment> getSegments() { return segments; }
     public int getStageID() { return stageID; }
