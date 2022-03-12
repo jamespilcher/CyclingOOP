13ddd1aeaf2d02f02d1052b45e6e4a9f7204f537
jamespilcher <90052874+jamespilcher@users.noreply.github.com>
2022-03-12 23:07:39 +0000

    stage sorting in getStage, (rename to 'getsortedstages'?)
    added stagestates too

--- cycling/Stage.java
+++ cycling/Stage.java
@@ -1,13 +1,14 @@
 package cycling;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
-class Stage {
-    private int stageID;
+class Stage extends IdHaver{
+    private int id;
     private int raceID; //parent race id
     private StageType type;
     private String stageName;
     private String description;
-    private double length;
+    private String stageState;
+    private Double length;
     private LocalDateTime startTime;
 
     ArrayList<Segment> segments = new ArrayList<Segment>();
@@ -24,21 +25,41 @@ class Stage {
         this.length = length;
         this.startTime = startTime;
         this.type = type;
-        this.stageID = ++numberOfStages;
+        id = ++numberOfStages;
+        stageState = "in preparation";
+        super.setId(id);
+
     }
 
     public void addSegment(Segment segment){
         segments.add(segment);
     }
-    
-    public ArrayList<Segment> getSegments() { return segments; }
-    public int getStageID() { return stageID; }
+
+    public void removeSegment(Segment segment){ 
+        segments.remove(segment);
+    }
+
+    public void deleteSegments(){ //not needed anymore
+        segments.clear();
+    }
+
+    public ArrayList<Segment> getSegments() { 
+        segments.sort((o1, o2)
+        -> o1.getLocation().compareTo(
+            o2.getLocation()));
+        return segments;
+    }
+        
+    public int getId() { return id; }
     public int getRaceID() { return raceID; }
     public StageType getType() { return type; }
     public String getStageName() { return stageName; }
     public String getDescription() { return description; }
     public double getLength() {return length;}
     public LocalDateTime getStartTime() { return startTime; }
-    public void deleteSegments(){ segments.clear();}
+    public String getStageState() { return stageState; }
 
+    public void setStageState(String stageState){
+        this.stageState = stageState;
+    }
 }
