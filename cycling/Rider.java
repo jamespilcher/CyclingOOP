36610885ad28147b739295ffb8d5212dbdb055cd
jamespilcher <90052874+jamespilcher@users.noreply.github.com>
2022-03-14 14:09:30 +0000

    Update Rider.java

--- cycling/Rider.java
+++ cycling/Rider.java
@@ -1,11 +1,18 @@
 package cycling;
+import java.util.ArrayList;
 public class Rider extends IdHaver{
     private int id;
     private String riderName;
     private int riderYearOfBirth;
     private int teamID;
 
+    private int totalPoints;
+    private int totalMountainPoints;
+
     private static int numberofRiders=0;
+
+    private ArrayList<RiderStageResults> riderResults = new ArrayList<RiderStageResults>();
+
 
     public Rider(int riderYearOfBirth, String riderName, int teamID){
         this.riderYearOfBirth=riderYearOfBirth;
@@ -13,6 +20,10 @@ public class Rider extends IdHaver{
         this.teamID=teamID;
         id=++numberofRiders;
         super.setId(id);
+    }
+
+    public void addStageResults(RiderStageResults rider){
+        riderResults.add(rider);
     }
 
     public int getTeamID() {return teamID;}
