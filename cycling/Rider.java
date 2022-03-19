900cdfa4096819f6dfee5ed36f9bf50bf544d5e2
jamespilcher <90052874+jamespilcher@users.noreply.github.com>
2022-03-19 14:55:27 +0000

    Update Rider.java

--- cycling/Rider.java
+++ cycling/Rider.java
@@ -1,18 +1,23 @@
 package cycling;
 import java.io.Serializable;
 import java.util.ArrayList;
-public class Rider extends IdHaver implements Serializable{
+public class Rider extends IdHaver implements Serializable {
+    
     private int id;
     private String riderName;
     private int riderYearOfBirth;
     private int teamID;
 
-    private int totalPoints;
-    private int totalMountainPoints;
+    private Long totalAdjustedTime = 0L;
+    private Long totalElapsedTime = 0L;
+
+
+    private int totalPoints = 0;
+    private int totalMountainPoints = 0;
 
     private static int numberofRiders=0;
 
-    private ArrayList<RiderStageResults> riderResults = new ArrayList<RiderStageResults>();
+    private ArrayList<RiderStageResults> riderResultsList = new ArrayList<RiderStageResults>();
 
 
     public Rider(int riderYearOfBirth, String riderName, int teamID){
@@ -24,10 +29,47 @@ public class Rider extends IdHaver implements Serializable{
     }
 
     public void addStageResults(RiderStageResults rider){
-        riderResults.add(rider);
+        riderResultsList.add(rider);
+    }
+
+    public void addTotalPoints(int points){
+        totalPoints += points;}
+    public void addTotalMountainPoints(int points){
+        totalMountainPoints += points;
+    }
+    public void addTotalAdjustedTime(Long time){
+        totalAdjustedTime += time;
+    }
+
+    public void addTotalElapsedTime(Long time){
+        totalElapsedTime += time;
     }
 
     public int getTeamID() {return teamID;}
     public void setTeamID(int teamID) {this.teamID=teamID;}
     public int getId() {return id;}
+
+    public Long getTotalElapsedTime(){
+        return totalElapsedTime;
+    }
+    public Long getTotalAdjustedTime(){
+        return totalAdjustedTime;
+    }
+
+    public ArrayList<RiderStageResults> getRiderResultsList() {return riderResultsList;}
+
+    public void setTotalAdjustedTime(Long time){
+        totalAdjustedTime=time;
+    }
+
+    public void setTotalElapsedTime(Long time){
+        totalElapsedTime=time;
+    }
+
+    public void setTotalPoints(int points) {totalPoints=points;}
+    public void setTotalMountainPoints(int points) {totalMountainPoints=points;}
+    
+    public int getTotalPoints() {return totalPoints;}
+    public int getTotalMountainPoints() {return totalMountainPoints;}
+
 }
