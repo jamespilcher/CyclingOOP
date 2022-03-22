ac59c4b5c221f59dc094b83063e7348916eecebe
Bahkbar <73535967+Bahkbar@users.noreply.github.com>
2022-03-22 18:38:13 +0000

    Update Stage.java
    Javadoc + small changes

--- cycling/Stage.java
+++ cycling/Stage.java
@@ -1,75 +1,147 @@
 package cycling;
+
 import java.io.Serializable;
 import java.time.LocalDateTime;
-import java.util.ArrayList;
+import java.util.LinkedList;
+
+/**
+ * Represents a stage within a race.
+ *
+ * @author James Pilcher
+ * @author Daniel Moulton
+ */
 public class Stage extends IdHaver implements Serializable {
-    private int id;
-    private int raceID; //parent race id
-    private StageType type;
-    private String stageName;
-    private String description;
-    private String stageState;
-    private Double length;
-    private LocalDateTime startTime;
+  private int id;
+  private int raceId; // ID of race the stage belongs to
+  private StageType type;
+  private String stageName;
+  private String description;
+  private String stageState; // Stage state i.e., waiting for results
+  private Double length;
+  private LocalDateTime startTime;
 
-    ArrayList<Segment> segments = new ArrayList<Segment>();
+  LinkedList<Segment> segments = new LinkedList<Segment>(); /* Linked list of segments 
+  within this stage*/
+
+  LinkedList<RiderStageResults> riderResultsList = 
+      new LinkedList<RiderStageResults>(); // Linked list of rider results in stage
+
+  private static int numberOfStages = 0; // Number of stages in portal.
+
+  /**
+   * Constructor for the Stage class.
+   *
+   * @param raceId ID of the race the stage belongs to.
+   * @param stageName Name of the stage
+   * @param description Description of the stage
+   * @param length Length of the stage in kilometers
+   * @param startTime Date and time of the start of the stage
+   * @param type The type of the stage (determines how many points winners get)
+   */
+  public Stage(int raceId, String stageName, String description, double length, 
+      LocalDateTime startTime, StageType type) { 
+    this.raceId = raceId;
+    this.stageName = stageName;
+    this.description = description;
+    this.length = length;
+    this.startTime = startTime;
+    this.type = type;
+    id = ++numberOfStages;
+    stageState = "in preparation";
+    super.setId(id);
+  }
+
+  /**
+   * Adds rider's results in stage to riderResultsList.
+   *
+   * @param rider Rider object of the corresponding rider 
+   */
+  public void addRiderResultToStage(RiderStageResults rider) { 
+    riderResultsList.add(rider);
+  }
+
+  /**
+   * Adds a segment to the stage (adds segment object to stage's list of segments).
+   *
+   * @param segment Segment object to be added.
+   */
+  public void addSegment(Segment segment) { 
+    segments.add(segment);
+  }
 
 
-    ArrayList<RiderStageResults> riderResultsList = new ArrayList<RiderStageResults>();
+  /**
+   * Removes specified segment object from stage's list of segments.
+   *
+   * @param segment Segment object to be removed.
+   */
+  public void removeSegment(Segment segment) { 
+    segments.remove(segment);
+  }
 
-    private static int numberOfStages = 0;
+  /**
+   * Removes all segments from the stage's list of segments.
+   */
+  public void deleteSegments() { 
+    segments.clear();
+  }
 
-
-
-    public Stage(int raceID, String stageName, String description, double length,
-        LocalDateTime startTime, StageType type){
-        this.raceID = raceID;
-        this.stageName = stageName;
-        this.description = description;
-        this.length = length;
-        this.startTime = startTime;
-        this.type = type;
-        id = ++numberOfStages;
-        stageState = "in preparation";
-        super.setId(id);
-
-    }
-
-    public void addRiderResultToStage(RiderStageResults rider){
-        riderResultsList.add(rider);
-    }
-
-    public void addSegment(Segment segment){
-        segments.add(segment);
-    }
-
-    public void removeSegment(Segment segment){ 
-        segments.remove(segment);
-    }
-
-    public void deleteSegments(){ //not needed anymore
-        segments.clear();
-    }
-
-    public ArrayList<Segment> getSegments() { 
-        segments.sort((o1, o2)
+  /**
+   * Sorts the list of segments by their position within the stage and returns the sorted list.
+   *
+   * @return List of segments ordered by their location/poistion in the stage.
+   */
+  public LinkedList<Segment> getSegments() { 
+    segments.sort((o1, o2)
         -> o1.getLocation().compareTo(
-            o2.getLocation()));
-        return segments;
-    }
-        
-    public int getId() { return id; }
-    public int getRaceID() { return raceID; }
-    public StageType getType() { return type; }
-    public String getStageName() { return stageName; }
-    public String getDescription() { return description; }
-    public double getLength() {return length;}
-    public LocalDateTime getStartTime() { return startTime; }
-    public String getStageState() { return stageState; }
-    public ArrayList<RiderStageResults> getRiderResultsList(){ return riderResultsList; }
+        o2.getLocation()));
+    return segments;
+  }
+      
+  public int getId() { 
+    return id; 
+  }
 
-    public void setStageState(String stageState){
-        this.stageState = stageState;
-    }
-    public static void resetID() {numberOfStages=0;}
+  public int getRaceId() { 
+    return raceId; 
+  }
+
+  public StageType getType() { 
+    return type;
+  }
+
+  public String getStageName() { 
+    return stageName;
+  }
+
+  public String getDescription() { 
+    return description;
+  }
+
+  public double getLength() { 
+    return length;
+  }
+
+  public LocalDateTime getStartTime() { 
+    return startTime; 
+  }
+
+  public String getStageState() { 
+    return stageState; 
+  }
+
+  public LinkedList<RiderStageResults> getRiderResultsList() { 
+    return riderResultsList; 
+  }
+
+  public void setStageState(String stageState) {
+    this.stageState = stageState;
+  }
+
+  /**
+   * Reset the number of stages, used when erasing the cycling portal.
+   */
+  public static void resetIdCounter() {
+    numberOfStages = 0;
+  }
 }
