7f1cf1848fa996a32a023f8b2ba02b144c76ca4d
Bahkbar <73535967+Bahkbar@users.noreply.github.com>
2022-03-23 01:21:32 +0000

    Update Segment.java
    Javadoc + comments + linting

--- cycling/Segment.java
+++ cycling/Segment.java
@@ -2,29 +2,55 @@ package cycling;
 
 import java.io.Serializable;
 
-//abstract class then override
-//time, length, 
+/**
+ * Represents a segment.
+ *
+ * @author James Pilcher
+ * @author Daniel Moulton
+ */
 public class Segment extends IdHaver implements Serializable {
-    private int id;
-    private int stageID;
-    private Double location;
-    private SegmentType type;
-    
-    private static int numberOfSegments = 0;
+  private int id;
+  private int stageId;
+  private Double location;
+  private SegmentType type;
+  
+  private static int numberOfSegments = 0;
 
+  /**
+   * Constructor for segment class.
+   *
+   * @param stageId ID of the stage the segment is a part of
+   * @param type The type of segment i.e. Sprint or HC
+   * @param location Where in the stage the segment occurs (in kilometers)
+   */
+  public Segment(int stageId, SegmentType type, double location) {
+    this.stageId = stageId;
+    this.type = type;
+    this.location = location;
+    id = ++numberOfSegments;
+    super.setId(id);
+  }
 
-    public Segment(int stageID, SegmentType type, double location){
-        this.stageID = stageID;
-        this.type = type;
-        this.location = location;
-        id = ++numberOfSegments;
-        super.setId(id);
+  public int getId() { 
+    return id; 
+  }
 
-    }
+  public Double getLocation() { 
+    return location;
+  }
 
-    public int getId() { return id; }
-    public int getStageID() { return stageID; }
-    public Double getLocation() { return location;}
-    public SegmentType getSegmentType() { return type; }
-    public static void resetID() {numberOfSegments=0;}
+  public SegmentType getSegmentType() { 
+    return type; 
+  }
+
+  public int getstageId() { 
+    return stageId; 
+  }
+
+  /**
+   * Resets the number of segments to 0, use when erasing cycling portal.
+   */
+  public static void resetIdCounter() {
+    numberOfSegments = 0;
+  }
 }
