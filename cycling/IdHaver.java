2af8d52997c3b1c554a90ca22019f686e599b78c
Bahkbar <73535967+Bahkbar@users.noreply.github.com>
2022-03-23 01:26:39 +0000

    Update IdHaver.java
    Linting+Javadoc

--- cycling/IdHaver.java
+++ cycling/IdHaver.java
@@ -1,8 +1,20 @@
 package cycling;
 
-public class IdHaver{
-    private int id;
-    public int getId() { return id;}
-    public void setId(int id) { this.id = id; }
+/**
+ * Contains attributes and methods to do with IDs, each class that needs an ID inherits this.
+ *
+ * @author James Pilcher
+ * @author Daniel Moulton
+ * @version 1.0
+ */
+public class IdHaver {
+  private int id;
 
+  public int getId() { 
+    return id;
+  }
+
+  public void setId(int id) { 
+    this.id = id;
+  }
 }
